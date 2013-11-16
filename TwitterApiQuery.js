1ff248995acf46a56fd725585f8223acfdeae0b4
jmluc123 <joshua.lucier@gmail.com>
2013-11-16 10:41:52 -0500

    Working slowly through twitter.

--- TwitterApiQuery.js
+++ TwitterApiQuery.js
@@ -1,7 +1,14 @@
 app.post('/twittergrab', function(req, res){
-
-    
-/* using twitter api directly blech
+/*twitter-js-client
+var config = {
+    consumerKey: twitterInfo.consumerKey,
+    consumerSecret: twitterInfo.consumerSecret,
+    accessToken: twitterInfo.accessToken,
+    accessTokenSecret: twitterInfo.accessTokenSecret
+}
+var twitter = new Twitter(config)
+*/
+// using twitter api directly blech
     var query = {
 		hash: req.body.SampleHash,
         oldest: req.body.oldest,
@@ -11,22 +18,38 @@ app.post('/twittergrab', function(req, res){
         latitude: req.body.latitude,
         longitude: req.body.longitude
 	};
-	var queryStringParts = {
-          
-    };
     
-    var queryStringPath = "https://api.twitter.com/1.1/search/tweets.json"
-    var queryString = "?q="+query.hash+"&geocode="+query.latitude+","+query.longitude+","+query.distance+"&count="+query.number+"&until="query.latest+"&since="+query.oldest;
+//query string for get request    
+var queryStringPath = "https://api.twitter.com/1.1/search/tweets.json"
+var queryString = encodeURIComponent("?q="+query.hash+"&geocode="+query.latitude+","+query.longitude+","+query.distance+"&count="+query.number+"&until="+query.latest+"&since="+query.oldest);
 
-var twitterQuery;
-    Authorization:
-OAuth oauth_consumer_key="DC0sePOBbQ8bYdC8r4Smg",oauth_signature_method="HMAC-SHA1",oauth_timestamp="1384562887",oauth_nonce="-229746174",oauth_version="1.0",oauth_token="416744660-eyAMGMAhI3ZfJK6F69QaFOBdJktWST7XbLHrFfCB",oauth_signature="xXCY4F0BSd8jhxHE3COtYVYuQZE%3D"
-https.request({hostname:'api.twitter.com',port: 443, path:"/1.1/search/tweets.json", method:"get"},function(res){
-    
-    
-});    
+//getting oauth2 token for access to the api  
+console.log("processing keys...");
+var cKey = encodeURIComponent(twitterInfo.consumerKey);  //uri encode
+var cSecret = encodeURIComponent(twitterInfo.consumerSecret); //uri encode
+var twitterAuth = cKey+':'+cSecret; //combine
+var twitterAuth = new Buffer(twitterAuth).toString('base64');   //convert to base64 
+console.log("processed keys...");
+
+console.log("preparing request data...");  
+var authPostData = {
+    "grant_type": "client_credentials"
+};
+authPostData = JSON.stringify(authPostData);
+var options = {"hostname":'api.twitter.com',"port": 443, "path":"/oauth2/token", "method":"post",
+headers: { "Host": 'api.twitter.com', "Authorization": "Basic "+twitterAuth,"Content-Type": "application/x-www-form-urlencoded;charset=UTF-8", "Content-Length": Buffer.byteLength(authPostData), "Accept-Encoding": "gzip"}}; //
+console.log("finished request data...preparing request");
+var authPostReq = https.request(options,function(res){
+    console.log(res);   //This is where I'm supposed to get token data back from oauth2
+});
+authPostReq.on('error', function(e){
+   console.log(e); 
+});
+console.log("finished request prep...posting request");
+authPostReq.write(authPostData);
+authPostReq.end();
+console.log("finished request");
     //https://twitter.com/search?q=%23dude%20near%3A44.016274%2C-73.166653%20within%3A15mi%20since%3A2010-02-01%20until%3A2010-03-01&src=typd
-    */
 	//http.get()
     /* Twitter.js stuff.  Twitter.js sucks so far
     var twitter = new twitterAPI({
@@ -52,7 +75,6 @@ https.request({hostname:'api.twitter.com',port: 443, path:"/1.1/search/tweets.js
 	*/
         //enter hash tags into databsase
         //get twitter requests from api with req.body
-    console.log(req.body);
     var userQuery = req.body;
     console.log(userQuery);
         
